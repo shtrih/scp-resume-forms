a0a8ab274c6ff638a2247be922d1fe5cfa9eacf4
shtrih <shtrih@hitagi.ru>
2019-03-28 06:08:06 +0400

    Request throttling

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -28,6 +28,7 @@ class Kernel extends HttpKernel
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            'throttle:10,1',
 //            \App\Http\Middleware\VerifyCsrfToken::class,
         ],
 
